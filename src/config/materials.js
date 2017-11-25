b377c0d0853cc6a4449eafd020d0d6a5fa53cbf8
Architektor <gloosx@gmail.com>
2017-11-26 01:18:06 +0200

    feat: materials

---
+++ src/config/materials.js
@@ -0,0 +1,30 @@
+/* Слоган под лого на главной */
+export const slogan = 'Никакой воды. Чисто цифры.';
+
+/* Название секции с работами */
+export const portfolioSectionName = 'Наши работы';
+
+/* Название секции с услугами */
+export const servicesSectionName = 'Услуги';
+
+/* Телефон */
+export const phoneNumber = '+7 (916) 239 21 11';
+
+/* Мыло */
+export const emailAddress = 'sales@inwhite.com';
+
+/* Адрес */
+export const address = {
+  city: 'Москва', // Город
+  street: 'Нижнеюрковская', // Улица
+  metroStation: 'Тараса Шевченко', // Метро
+  houseNumber: '31', // Номер дома
+  additionalString: 'стр 33, подьезд 1', // Дополнительная инфа
+};
+
+/* Ссылки на соцсети (http:// обязательно)*/
+export const socialLinks = {
+  vkLink: 'http://vk.com/durov',
+  instagramLink: 'http://instagram.com/durov',
+  facebookLink: 'http://facebook.com/durov',
+};
